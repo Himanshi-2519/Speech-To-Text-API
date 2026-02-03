c474df32c1ffe6653cc6314d56942803a44886e8
HVM_110 <202296546+Himanshi-2519@users.noreply.github.com>
2026-02-03 23:07:59 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -125,7 +125,4 @@ app.get('/api/history', async (req, res) => {
        res.json({ message: "Cleared" });
       });
 
-        const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
-//app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+  app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
